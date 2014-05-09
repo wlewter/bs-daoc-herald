ebda74467aa99ee83bf09118492408c242a2f860
Wayne Lewter <wlewter@gmail.com>
2014-05-09 08:42:06 -0500

    fix jshint double quote problem

--- app/scripts/directives/pressenter.js
+++ app/scripts/directives/pressenter.js
@@ -4,7 +4,7 @@
   angular.module('bsDaocHeraldApp')
     .directive('bsEnter', function() {
       return function(scope, element, attrs) {
-        element.bind("keydown keypress", function(event) {
+        element.bind('keydown keypress', function(event) {
           if(event.which === 13) {
             scope.$apply(function(){
               scope.$eval(attrs.bsEnter, {'event': event});
